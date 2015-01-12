8550440fe8ea1f9fd51d82dbdb7d5244efe06fae
mtsampaio <marcost.sampaio@gmail.com>
2015-01-12 21:47:50 -0200

    Missing initial commit files

---
+++ Chi.SocialNetwork/Chi.SocialNetwork/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Chi.SocialNetwork.Data;
+using System.Web.Mvc;
+
+namespace Chi.SocialNetwork.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Repository rep;
+
+        public HomeController()
+        {
+            this.rep = new Repository();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "CHI Social Network";
+
+            this.rep.InsertUser(new User());
+
+            return View();
+        }
+    }
+}
---
+++ Chi.SocialNetwork/Chi.SocialNetwork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chi.SocialNetwork.WebApiApplication" Language="C#" %>
